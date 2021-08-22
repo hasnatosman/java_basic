7d31484a1415f1ff001528dd145218e4d3bf64f1
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-08-21 21:50:58 -0700

    Even and Odd
    Checking even or odd.

---
+++ even_odd.java
@@ -0,0 +1,9 @@
+public class even_odd{
+	public static void main(String args[]){
+		int x = 9;
+		if ( x % 2 == 0)
+		System.out.println("It is an even number!");
+		else
+		System.out.println("It is an odd number!");
+	}
+}
