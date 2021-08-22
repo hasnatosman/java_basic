c3db1c497eff669cb91dcc725bc735a16340c4e4
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-08-21 22:08:00 -0700

    Addition
    sum of two number

---
+++ addition.java
@@ -0,0 +1,9 @@
+class addition{
+	public static void main(String args[]){
+		int num1, num2, total;
+		num1 = 10;
+		num2 = 15;
+		total = num1 + num2;
+		System.out.println("Addition of two number: " + total);
+	}
+}
