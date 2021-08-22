ee4d94392619cf958e2cbd0cd4e424eb143a9294
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-08-21 21:52:02 -0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,14 @@
+public class test{
+public static void main(String args[])
+{
+int num = 10;
+for (int i = 1; i <= num; i++) {
+    if (i % 2 == 0) {
+        System.out.print("\n" + i + " Is Even");
+    } else {
+        System.out.print("\n" + i + " Is Odd");
+    }
+}
+
+}
+}
