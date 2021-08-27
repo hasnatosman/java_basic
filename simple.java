628db72af2694ee0ab0abdf06176cafeaf746e6c
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-08-27 01:36:19 -0700

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,26 @@
+/*if(condition) {    
+statement 1; //executes when condition is true   
+}
+
+if(condition) {    
+statement 1; //executes when condition is true   
+}  
+else{  
+statement 2; //executes when condition is false   
+}  
+*/
+
+
+
+class ifState{
+	public static void main(String args[]){
+		int x = 5, y = 10;
+		if( x + y < 10){
+			System.out.println(" It is right!");
+		}
+		else{
+			System.out.println("It is wrong!");
+		}
+		
+	}
+}
