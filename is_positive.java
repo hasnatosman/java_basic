842808c77f79b611da0623dddf41065c7e21e271
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-08-31 09:31:16 -0700

    Add files via upload

---
+++ is_positive.java
@@ -0,0 +1,11 @@
+class is_positive{
+	public static void main(String args[]){
+		int x = -1;
+		if(x < 0){
+			System.out.println("X is a negetive number!");
+		}
+		else{
+			System.out.println("X is a positive number!");
+		}
+	}
+}
