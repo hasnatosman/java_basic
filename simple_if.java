c0120a89afd75330b1dc57b7615894dd3284e0a3
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-08-31 00:19:54 -0700

    Add files via upload

---
+++ simple_if.java
@@ -0,0 +1,17 @@
+public class simple_if{
+
+public static void main(String args[]){
+	int x, y;
+	x = 20;
+	y = 20;
+	if (x  < y){
+		System.out.println("X is less than y!!");
+	}
+	else if(x > y){
+		System.out.println("X is greater than y!!");
+	}
+	else{
+		System.out.println("X is equal to y!!");
+	}
+}
+}
