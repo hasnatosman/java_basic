605a07d4e5d3d9503a95ad727beec2cd620b4285
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-08-31 07:30:30 -0700

    Add files via upload

---
+++ natural_number1.java
@@ -0,0 +1,10 @@
+class natural_number1{
+	public static void main(String args[]){
+		int i = 1;
+		
+		while (i <= 10){
+			System.out.println("First 10 natural numbers:"+i);
+			i = i + 1;
+		}
+	}
+}
