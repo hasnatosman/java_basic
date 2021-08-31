afd07336ffe544b6f5e5d62dc80b3dc928bad108
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-08-31 09:30:21 -0700

    Add files via upload

--- even_odd.java
+++ even_odd.java
@@ -1,9 +1,11 @@
-public class even_odd{
+class even_odd{
 	public static void main(String args[]){
-		int x = 9;
-		if ( x % 2 == 0)
-		System.out.println("It is an even number!");
-		else
-		System.out.println("It is an odd number!");
+		int num = 11;
+		if (num % 2 == 0){
+			System.out.println("It is an even number!");
+		}
+		else{
+			System.out.println("It is an odd number!");
+		}
 	}
 }
