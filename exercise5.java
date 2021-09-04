c682ec82912415e308485e5b9353a68640f14f33
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-04 10:31:29 -0700

    Add files via upload

---
+++ exercise5.java
@@ -0,0 +1,12 @@
+// Given x, find x3+x2+x.
+
+class exercise5{
+	public static void main(String args[]){
+		double x = 2;
+		double x3 = Math.pow(x, 3);
+		double x2 = Math.pow(x, 2);
+		double result = x3+x2+x;
+		
+		System.out.println("Result is: "+result);
+	}
+}
