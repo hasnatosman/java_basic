6b6f2260f42adfc0dfcf09ff981b9748fa0082f8
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-03 22:29:50 -0700

    Add files via upload

---
+++ exercise3.java
@@ -0,0 +1,20 @@
+//Given x find cube of x.(Hint Math.pow)
+
+class exercise3{
+	public static void main(String args[]){
+		
+		// square of x
+		double x = Math.pow(25, 2);
+		
+		// square root of y
+		double y = Math.sqrt(25);
+		
+		// cube of z
+		double z = Math.pow(3,3);
+		
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+}
