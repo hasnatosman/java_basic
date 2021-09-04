4851eaa3fd763a0d15a5efb141efbac6134ff13f
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-03 22:23:00 -0700

    Add files via upload

---
+++ exercise.java
@@ -0,0 +1,14 @@
+ // Find what is x percent of y.
+ 
+class exercise{
+	public static void main(String args[]){
+		// what is x perchent of y
+		// here, y = 80 and x = 10
+		// formula of percent is: (x/y)*100
+		double x = 10;
+		double y = 80;
+		double x_percent_of_y = (x/100)*y;
+		System.out.println(x_percent_of_y);
+		
+	}
+}
