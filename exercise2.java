51bfbc3a4ca11862c6bbfa018df8278a94cb4b37
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-03 22:29:32 -0700

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,14 @@
+//Find what is z% of y.
+
+class exercise2{
+	public static void main(String args[]){
+		// what is x perchent of y
+		// here, y = 80 and x = 10
+		// formula of percent is: (x/y)*100
+		double x = 20;
+		double y = 200;
+		double x_percent_of_y = (x/100)*y;
+		System.out.println(x_percent_of_y);
+		
+	}
+}
