7f7a4725a0d84f75592bd7298f0e86b47da298eb
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-06 01:59:57 -0700

    Add files via upload

---
+++ exercise9.java
@@ -0,0 +1,12 @@
+class exercise9{
+	public static void main(String args[]){
+		int total = 0;
+		for (int i = 0; i <= 5; i++){
+			if(i % 2 != 0){
+				total = total + i;
+			}
+			
+		}
+		System.out.println(total);
+	}
+}
