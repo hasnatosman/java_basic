78a9cf25cf02ec6c2d8461fdb3866bbc3e1dee4f
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-08 21:19:43 -0700

    Add files via upload

---
+++ if_else2.java
@@ -0,0 +1,12 @@
+class if_else2{
+	public static void main(String args[]){
+		String name = "HASNAT";
+		if(name == "HASNAT"){
+			System.out.println("He is 25 years old!");
+		
+		}
+		else{
+			System.out.println("He is 100 years old!");
+		}
+	}
+}
