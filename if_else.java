fd0784456d0eac9aeda20b3357bb4ff13449c2c1
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-08 21:19:23 -0700

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,9 @@
+class if_else{
+	public static void main(String args[]){
+		String name = "HASNAT";
+		System.out.println(name);
+		System.out.println("My name is "+name);
+		// HASNAT is my name.
+		System.out.println(name+" is my name!");
+	}
+}
