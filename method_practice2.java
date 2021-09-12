ebb49d65dde88a78bf8b2e8d24611b3610859ef6
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-11 23:09:30 -0700

    Add files via upload

---
+++ method_practice2.java
@@ -0,0 +1,15 @@
+class method_practice2{
+	public static void main(String args[]){
+		System.out.println("Hi! HASNAT !!!");
+		System.out.println("Maximum number is: "+comp(7,9));
+	}
+	public static int comp(int x, int y){
+		if( x < y){
+			return y;
+		}
+		else{
+			return x;
+		}
+	}
+	
+}
