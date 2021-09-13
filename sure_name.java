f76218806b6a6731cfea805a8779b70e9d46c721
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-13 02:23:50 -0700

    Add files via upload

---
+++ sure_name.java
@@ -0,0 +1,13 @@
+// input fName, lNname, print fullName
+
+class sure_name{
+	public static void main(String args[]){
+		System.out.println(full_name("Hasnat", "Osman"));
+		
+	}
+	public static String full_name(String fName, String lName){
+		//fName = "Hasnat";
+		//lName = "Osman";
+		return fName + " "+ lName;
+	}
+}
