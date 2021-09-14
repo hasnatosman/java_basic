b0226101859fd99aa296f065351ee869aff9ad01
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-14 01:58:05 -0700

    Add files via upload

---
+++ func_math.java
@@ -0,0 +1,10 @@
+//find what is x % of y
+class func_math{
+	public static void main(String args[]){
+		System.out.println(percent(20, 200));
+		
+	}
+	public static int percent(int x, int y){
+		return x * y / 100;
+	}
+}
