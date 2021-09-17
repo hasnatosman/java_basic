c0e79919ec0a767f8c16b8abad464a77955e183e
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-17 09:39:03 -0700

    Add files via upload

---
+++ student.java
@@ -0,0 +1,12 @@
+public class student{
+	//String fName;
+	//String lName;
+	
+	
+	public static void main(String args[]){
+		System.out.println(fullName("Hasnat ", "Osman"));
+	}
+	public static String fullName(String fName, String lName){
+		return fName + lName;
+	}
+}
