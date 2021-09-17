79670755d4889554c61c5edd5263f2990ea5423f
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-17 09:45:34 -0700

    Add files via upload

--- exercise.java
+++ exercise.java
@@ -4,10 +4,10 @@ class exercise{
 	public static void main(String args[]){
 		// what is x perchent of y
 		// here, y = 80 and x = 10
-		// formula of percent is: (x/y)*100
+		// formula of percent is: (x*y)/100
 		double x = 10;
 		double y = 80;
-		double x_percent_of_y = (x/100)*y;
+		double x_percent_of_y = (x*y)/100;
 		System.out.println(x_percent_of_y);
 		
 	}
