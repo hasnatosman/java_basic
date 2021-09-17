114921f737f23b9d5e9d610376bba291c7f4447d
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-17 09:44:43 -0700

    Add files via upload

---
+++ method_definition.java
@@ -0,0 +1,27 @@
+class method_definition{
+	public static void main(String args[]){
+		/*
+		A. What is method?
+			Method is:
+				> collection of statements
+				> perform specific task
+				> return the result to the caller
+				> return nothing
+				> can reuse it later
+		
+		B. How to declare a method?
+			In general there are six types of components.
+			
+			Modifier:
+			>> defines access type of the method
+			>> from where it can be accessed in your application
+				1. public
+				2. protected
+				3. private
+				4. default
+			
+			Return type:
+			
+		*/
+	}
+}
