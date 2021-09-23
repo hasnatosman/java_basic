cb54811c4d0483ff4b0d06647cb0de288032720d
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-23 10:56:15 -0700

    Add files via upload

---
+++ math_1.java
@@ -0,0 +1,11 @@
+public class math_1{
+	public static void main(String args[]){
+		System.out.println("Maximum between x and y is: "+MathH.max(11, 9));
+		System.out.println("Taka to doller: "+MathH.taka_to_doller(200, 80));
+		System.out.println("Feet to meter: "+MathH.feet_to_meter(100));
+		System.out.println("Meter to feet: "+MathH.meter_to_feet(100));
+		System.out.println("Kg to pound: "+MathH.kg_to_pound(20));
+		System.out.println("Pound to kg: "+MathH.pound_to_kg(20));
+		
+	}
+}
