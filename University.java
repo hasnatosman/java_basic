6f5bd1ae2ec28feb1f2b9d3cb0d5a03941f7a5cc
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-09-28 23:23:28 -0700

    Add files via upload

---
+++ University.java
@@ -0,0 +1,11 @@
+//class university with attribute name,total student,city,shrotcode.
+//class student with attribute name,id,university name,email.
+//class utility method name generate_email,short_code,id.
+//email format s26@bubt.com
+
+public class University{
+	String name;
+	String total_student;
+	String city;
+	String short_code;
+}
