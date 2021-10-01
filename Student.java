cab230cbc901ebe09c19ac4f187224e47b45144a
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-10-01 01:30:43 -0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student{
+	String name;
+	String surename;
+	int id;
+	String email;
+	String universityCode;
+	
+	public String toString(){
+		//Tareq Saifullah id no 12 of university DU his email is s24@du.com
+		return name +" "+surename+ " id no " + id+ " of university " +universityCode+ " his email is "+ email;
+	}
+}
