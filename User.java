c2c7464a872dd7f0f07b5970697d2ae2cd71b62b
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-10-01 01:31:11 -0700

    Add files via upload

--- User.java
+++ User.java
@@ -1,6 +1,17 @@
 public class User{
-	String first_name;
-	String last_name;
-	String email;
-	String password;
+	public static void main(String args[]){
+	Student tarek = new Student();
+	tarek.name = "Tarek";
+	tarek.surename = "Saifullah";
+	tarek.id = 12;
+	tarek.universityCode = "DU";
+	tarek.email = Utility.emailGenerator(tarek.universityCode, tarek.id);
+	
+	//Tareq Saifullah id no 12 of university DU his email is s24@du.com
+	
+	System.out.println(tarek.name +" "+ tarek.surename + " id no " + tarek.id + " of university "+ tarek.universityCode+" his email is "+ tarek.email);
+	System.out.println(tarek.toString());
+	
+	}
+	
 }
