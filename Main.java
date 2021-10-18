32595aae1fb30c7e362fe167000092ee5cc81976
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-10-18 09:48:19 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{
+	public static void main(String args[]){
+		Programmer p = new Programmer();
+		System.out.println(p.salary);
+		System.out.println(p.bonus);
+		System.out.println(p.salary+p.bonus);
+	}
+}
